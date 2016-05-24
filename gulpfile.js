b24dce3bd5c86dbe7d60c271f1567233a0565000
Abdul Basit <abdulbasit@Abduls-MacBook-Pro.local>
2016-05-24 14:49:42 +0500

    setup of gulp scripts

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+
+gulp.task('sass', function(){
+  return gulp.src('app/scss/**/*.scss')
+          .pipe(sass())
+          .pipe(gulp.dest('app/css'))
+          .pipe(browserSync.reload({
+            stream:true
+          }))
+});
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+        .pipe(useref())
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('watch', ['browserSync','sass'], function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+
+});
+
+gulp.task('browserSync', function(){
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+});
+gulp.task('hello', function(){
+  console.log('hello world');
+});
