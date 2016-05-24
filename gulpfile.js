2c3765a604203c965ad906001562bc07ba18c70d
Abdul Basit <abdulbasit@Abduls-MacBook-Pro.local>
2016-05-24 16:07:36 +0500

    Gulping up the project

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpif = require('gulp-if');
+var cssnano =  require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache =  require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
 
 gulp.task('sass', function(){
   return gulp.src('app/scss/**/*.scss')
@@ -15,7 +22,26 @@ gulp.task('sass', function(){
 gulp.task('useref', function(){
   return gulp.src('app/*.html')
         .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', cssnano()))
         .pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', function(){
+  return gulp.src('app/images/**/*.+(jpg|svg|gif|png)')
+        .pipe(cache(imagemin({
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('fonts', function(){
+  return gulp.src('app/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('clean:dist', function(){
+  return del.sync('dist');
 });
 
 gulp.task('watch', ['browserSync','sass'], function(){
@@ -32,6 +58,16 @@ gulp.task('browserSync', function(){
     },
   })
 });
-gulp.task('hello', function(){
-  console.log('hello world');
+
+gulp.task('build', function(callback){
+  runSequence('clean:dist',
+    ['sass', 'useref', 'images', 'fonts'],
+    callback
+    )
 });
+
+gulp.task('default', function(callback){
+  runSequence(['sass', 'browserSync', 'watch'],
+    callback
+    )
+})
