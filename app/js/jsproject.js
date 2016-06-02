28289a13ef23fe2689c8d999cd6b9aff73c7fadd
dewsoftpvtltd <abdulbasitlangrial@outlook.com>
2016-06-02 10:37:15 +0500

    project and componeent

--- app/js/jsproject.js
+++ app/js/jsproject.js
@@ -1,30 +1,85 @@
-angular.module('jsApp', ['ngMaterial', 'ngMessages'])
-  .controller('TodoListController', function() {
-    var todoList = this;
-    todoList.todos = [
-      {text:'learn angular', done:true},
-      {text:'build an angular app', done:false},
-      {text:'correct your layout', done:false},
-      {text:'find suitable checkpoints for responsiveness', done:false}];
+angular.module('jsApp', ['ngMaterial', 'ngMessages','ngRoute','restangular'])
 
-    todoList.addTodo = function() {
-      todoList.todos.push({text:todoList.todoText, done:false});
-      todoList.todoText = '';
-    };
-
-    todoList.remaining = function() {
-      var count = 0;
-      angular.forEach(todoList.todos, function(todo) {
-        count += todo.done ? 0 : 1;
+  .config(function($routeProvider, RestangularProvider) {
+    $routeProvider.
+      when('/', {
+        controller:ListCtrl,
+        templateUrl:'partials/list.html'
+      }).
+      when('/edit/:projectId', {
+        controller:EditCtrl,
+        templateUrl:'partials/detail.html',
+        resolve: {
+          project: function(Restangular, $route){
+            return Restangular.one('drink', $route.current.params.projectId).get();
+          }
+        }
+      }).
+      when('/new', {
+        controller:CreateCtrl,
+        templateUrl:'partials/detail.html'
+      }).
+      otherwise({
+        redirectTo:'/'
       });
-      return count;
-    };
 
-    todoList.archive = function() {
-      var oldTodos = todoList.todos;
-      todoList.todos = [];
-      angular.forEach(oldTodos, function(todo) {
-        if (!todo.done) todoList.todos.push(todo);
-      });
-    };
+      RestangularProvider.setBaseUrl('http://testing.app:8000/api/v1');
+     // RestangularProvider.setDefaultHttpFields({cache: true});
+
+      //RestangularProvider.setDefaultRequestParams({ apiKey: '4f847ad3e4b08a2eed5f3b54' })
+     // RestangularProvider.setRestangularFields({
+     //   id: '_id.$oid'
+    //  });
+
+      RestangularProvider.setRequestInterceptor(function(elem, operation, what) {
+
+        if (operation === 'put') {
+          elem._id = undefined;
+          return elem;
+        }
+        return elem;
+      })
   });
+
+
+function ListCtrl($scope, Restangular) {
+   $scope.projects = Restangular.all("drink").getList().$object;
+}
+
+
+function CreateCtrl($scope, $location, Restangular) {
+  $scope.save = function() {
+    Restangular.all('drink').post($scope.project).then(function(project) {
+      $location.path('/list');
+    });
+  }
+   $scope.cancel = function() {
+      $location.path('/');
+  }
+}
+
+function EditCtrl($scope, $location, Restangular, project) {
+  var original = project;
+  $scope.project = Restangular.copy(original);
+
+
+  $scope.isClean = function() {
+    return angular.equals(original, $scope.project);
+  }
+
+  $scope.destroy = function() {
+    original.remove().then(function() {
+      $location.path('/list');
+    });
+  };
+
+  $scope.save = function() {
+    $scope.project.put().then(function() {
+      $location.path('/');
+    });
+  };
+  $scope.cancel = function() {
+      $location.path('/');
+  };
+}
+
